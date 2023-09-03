748abb818c54d5c4dc40b1a8a25bfdc4c54a1bd5
枣狸 <zlols@foxmail.com>
2023-09-03 22:25:39 +0800

    control module update

--- run.py
+++ run.py
@@ -3,7 +3,8 @@ cid = 'android0'
 
 import time, keyboard
 
-import utils.autoandroid3 as autoandroid
+#import utils.autoandroid3 as autoandroid
+import anhelper
 
 import hottaui
 
@@ -11,8 +12,40 @@ from motions.autointeract import autoInteract
 from motions.exitparty import exitParty
 from motions.announce import guildAnnounceEN, worldAnnounceEN
 
-if __name__ == '__main__':
+class AutoAndroidCompatible:
+    def __init__(self):
+        self.dev = anhelper.device.getDevice()
+        print(self.dev)
+        self.dev.setWM(**anhelper.device.CONST.TABLET_720P, fit_orientation = True)
+        self.minicap = anhelper.minicap.getMinicap(self.dev.id)
+        self.minitouch = anhelper.minitouch.getMinitouch(self.dev.id)
+        self.touch = self.Touch(self.minitouch)
+        self.ime = anhelper.ime.getIME(self.dev.id)
+        self.minicap.start()
+        self.minitouch.start()
+        self.ime.ensure()
+        
+    def cap(self):
+        return self.minicap.cap()
+        
+    def inputchar(self, txt):  
+        self.ime.inputSafe(txt)
+        
+    class Touch:
+        
+        def __init__(self, minitouch):
+            self.minitouch = minitouch
+        
+        def tap(self, x, y):
+            with self.minitouch.useContact() as c:
+                c.tap(x, y)
+        
+    
+        
 
+if __name__ == '__main__':
+    print(anhelper.device.listDevices())
+    autoandroid = AutoAndroidCompatible()
     ti = 0
     tt = time.time()
     lastMissionExitTs = 0
@@ -47,7 +80,8 @@ if __name__ == '__main__':
             
             
             elif idle and time.time()-lastAnnounceTs>1800:
-                if guildAnnounceEN(globals(), 'Self-service_Bot_Beta_:p'):
+                #if guildAnnounceEN(globals(), '回归自动机，点击邀请组队即可使用（）'):
+                if worldAnnounceEN(globals(), '回归自动机，点击邀请组队即可使用（）'):
                     lastAnnounceTs = time.time()
                 
             
@@ -58,7 +92,8 @@ if __name__ == '__main__':
                         lastExitPartyTs = time.time()
                         idle = True
                 
-            print(f'\r采样帧率：{autoandroid.minicap.realfps:.2f}, 待机状态：{idle}  ', end='')
+            #print(f'\r采样帧率：{autoandroid.minicap.realfps:.2f}, 待机状态：{idle}  ', end='')
+            print(f'\r待机状态：{idle}  ', end='')
             
             ti = time.time()
             time.sleep(0.1)
